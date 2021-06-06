3eed6e6281d3bc1e394a84906365ebd7001c2718
Sukhmandeep-Maan <85410928+Sukhmandeep-Maan@users.noreply.github.com>
2021-06-06 11:47:00 +0530

    Add files via upload

---
+++ dice stimulator.py
@@ -0,0 +1,9 @@
+import random
+check = True
+while check:
+    print(random.randint(1,6))
+    want_to_continue=input("Want to continue the game(y/n): ")
+    if want_to_continue.lower()=='y':f
+        continue
+    else:
+        break
